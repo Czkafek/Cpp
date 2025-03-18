88903ed54eddbd573c2ea4727ad2df4f1bb20d75
Czkafek <czkafek@gmail.com>
2025-03-18 21:40:04 +0100

    Some exercises with cpp

---
+++ Szkolacpp/mati/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class BMI {
+private:
+	double weight, height;
+public:
+
+	BMI() { weight = 1; height = 1; }
+	BMI(double weight, double height) : BMI() {
+		SetW(weight);
+		SetH(height);
+	}
+
+	void Show() { cout << "\nBMI [ weight: " << weight << ", height: " << height << ", BMI: " << GetBMI() << " ]"; }
+
+	double GetBMI() { return weight / (height * height); }
+		 
+	void SetW(double weight) { 
+		if (weight > 0)
+			this->weight = weight;
+	}
+	void SetH(double height) {
+		if (height > 0)
+			this->height = height;
+	}
+	double W() { return weight; }
+	double H() { return height; }
+};
+
+int main()
+{
+	BMI b1, b2(75, 1.9), b3(-2178, -324);
+	b1.Show();
+	b2.Show();
+	b3.Show();
+	int a;
+
+
+	_getch();
+}
---
+++ Szkolacpp/workApp/workApp.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Polish");
+	double a, b;
+	cout << "Podaj pierwszą liczbę: ";
+	cin >> a;
+	cout << "Podaj drugą liczbę: ";
+	cin >> b;
+
+	a > b ? cout << "Pierwsza liczba jest większa\n" : cout << "Druga liczba jest większa\n";
+
+	if (b != 0)
+		cout << "Wynik z dzielenia: " << a / b;
+	else
+		cout << "Nie dzieli się przez 0 idiot jebany!";
+
+	_getch();
+}
