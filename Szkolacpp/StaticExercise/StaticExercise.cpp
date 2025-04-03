0a92f44f9f90023f241c40870084131e6f9296e0
Czkafek <czkafek@gmail.com>
2025-04-03 23:17:13 +0200

    Small exercise

---
+++ Szkolacpp/StaticExercise/StaticExercise.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class BMI {
+private:
+    double weight, height;
+public:
+    double static GetBMI(double weight, double height);
+};
+
+double BMI::GetBMI(double weight, double height) {
+    return weight / (height * height);
+};
+
+int main()
+{
+
+    cout << BMI::GetBMI(75, 1.9);
+
+    _getch();
+}
